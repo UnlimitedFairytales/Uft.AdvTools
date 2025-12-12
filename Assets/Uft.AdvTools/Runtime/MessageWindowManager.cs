157b4e490a294b82f97eb69b9d78658230eaf368
SNOW\Ether <etherealgarden@msn.com>
2025-12-12 12:30:13 +0900

    コメント整理

--- Assets/Uft.AdvTools/Runtime/MessageWindowManager.cs
+++ Assets/Uft.AdvTools/Runtime/MessageWindowManager.cs
@@ -4,9 +4,13 @@ using Uft.AdvTools.View;
 
 namespace Uft.AdvTools
 {
+    /// <summary>
+    /// 1. 非表示Windowへフォーカスする場合、直前のWindowは非表示される<br/>
+    /// 2. 表示Window間へフォーカスする場合、直前のWindowはそのまま
+    /// </summary>
     public class MessageWindowManager
     {
-        public MessageWindow? MessageWindow { get; protected set; } // NOTE: Setup() 自動検出
+        public MessageWindow? MessageWindow { get; protected set; }
 
         public void Setup(MessageWindow[] messageWindowList)
         {
