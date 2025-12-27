108bf5069be8c2b235d330a6ef5d79ce6450c5f0
SNOW\Ether <etherealgarden@msn.com>
2025-12-27 20:23:50 +0900

    準備

--- Assets/Uft.AdvTools/Runtime/View/OffsettableImage.cs
+++ Assets/Uft.AdvTools/Runtime/View/OffsettableImage.cs
@@ -12,6 +12,7 @@ namespace Uft.AdvTools.View
     {
         // Parameters
 
+        [SerializeField] protected CanvasGroup? _canvasGroup; public CanvasGroup CanvasGroup => this._canvasGroup!; // NOTE: null-forgiving（警告抑制のみ）。他のメンバで必須チェック済み。
         [SerializeField] protected Image? _image; public Image Image => this._image!; // NOTE: null-forgiving（警告抑制のみ）。他のメンバで必須チェック済み。
 
         public RectTransform RootRectTransform => (RectTransform)this.transform;
@@ -21,13 +22,15 @@ namespace Uft.AdvTools.View
 
         void Reset()
         {
+            this._canvasGroup = this.GetComponentInChildren<CanvasGroup>(true);
             this._image = this.GetComponentInChildren<Image>(true);
         }
 
         // Methods
 
-        public void Set(bool isOn, Sprite? sprite, Vector2 pivot, float scale, Vector2 position, Vector2 offset, Color fromColor, Color toColor, float fadeTime_sec)
+        public virtual void Set(bool isOn, Sprite? sprite, Vector2 pivot, float scale, Vector2 position, Vector2 offset, Color fromColor, Color toColor, float fadeTime_sec)
         {
+            if (this._canvasGroup == null) throw new InvalidOperationException($"{nameof(this._canvasGroup)} is required.");
             if (this._image == null) throw new InvalidOperationException($"{nameof(this._image)} is required.");
 
             this.IsOn = isOn;
@@ -46,8 +49,9 @@ namespace Uft.AdvTools.View
             img.DOColor(toColor, fadeTime_sec);
         }
 
-        public void Off(Color toColor, float fadeTime_sec)
+        public virtual void Off(Color toColor, float fadeTime_sec)
         {
+            if (this._canvasGroup == null) throw new InvalidOperationException($"{nameof(this._canvasGroup)} is required.");
             if (this._image == null) throw new InvalidOperationException($"{nameof(this._image)} is required.");
 
             this.IsOn = false;
