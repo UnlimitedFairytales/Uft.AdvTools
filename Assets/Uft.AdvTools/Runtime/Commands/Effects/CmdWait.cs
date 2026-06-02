6057f6b6c599f46ff4882291a6676a544c9d4a78
SNOW\Ether <etherealgarden@msn.com>
2026-06-02 23:10:29 +0900

    async・ct対応5

--- Assets/Uft.AdvTools/Runtime/Commands/Effects/CmdWait.cs
+++ Assets/Uft.AdvTools/Runtime/Commands/Effects/CmdWait.cs
@@ -18,11 +18,12 @@ namespace Uft.AdvTools.Commands
         public virtual void Run(ScenarioExecutor scenarioExecutor, AdvRoot advRoot)
         {
             scenarioExecutor.IsWaiting = true;
+            var ct = advRoot.destroyCancellationToken;
             UniTask.Void(async () =>
             {
                 try
                 {
-                    await UniTask.Delay((int)(this.WaitSeconds * 1000));
+                    await UniTask.Delay((int)(this.WaitSeconds * 1000), cancellationToken: ct);
                 }
                 finally
                 {
